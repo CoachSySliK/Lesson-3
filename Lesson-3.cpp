b776589b5bdfff4f9d3d8e5a9ae07b2fe69fff18
hp victus <hp victus@LAPTOP-2I0QRUOT>
2024-04-03 17:16:40 +0300

    Добавьте файлы проекта.

---
+++ Lesson-3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int a;
+	cout << "\"Возвеление числа в квадрат\"\n";
+	cout << "Введите число ";
+	cin >> a;
+	cout << "Квадрат числа " << a << " равен " << a * a;
+
+
+	return 0;
+}
